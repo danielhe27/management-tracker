9976783f62e84c5c5be9e9baf779912610b5cb1e
danielhe27 <jdah49d@gmail.com>
2024-03-12 18:16:31 -0400

    cleaning and recreating code

--- dev/server.js
+++ dev/server.js
@@ -1,11 +1,9 @@
-const express = require('express');
+
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+require('dotenv').config();
 
 const db = mysql.createConnection(
 {
@@ -17,7 +15,53 @@ const db = mysql.createConnection(
 console.log('Connected to the super_db database.')
 );
 
+const promptUser = () => {
+inquirer
+   .prompt([
+      {
+        type: 'list',
+        name: 'options',
+        message: 'What would you like to see?',
+        choices: [
+          'View All Employees',
+          'Add Employee',
+          'Update Employee Role',
+          'View All Roles',
+          'Add Role',
+          'View All Departments',
+          'Add Department',
+          'Exit'
+        ]
+      }
+    ])
+   .then((answer) => {
+      switch (answer.action) {
+        case 'View All Employees':
+          viewAllEmployees();
+          break;
+        case 'Add Employee':
+          addEmployee();
+          break;
+        case 'Update Employee Role':
+          updateEmployeeRole();
+          break;
+        case 'View All Roles':
+          viewAllRoles();
+          break;
+        case 'Add Role':
+          addRole();
+          break;
+        case 'View All Departments':
+          viewAllDepartments();
+          break;
+        case 'Add Department':
+          addDepartment();
+          break;
+        case 'Exit':
+          process.exit();
+          console.log('Goodbye!');
+      }
+    });
+    };
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    ShowAll
