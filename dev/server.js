b1550d31852a6ca3bbb5ee176041bee143d7bf9e
danielhe27 <jdah49d@gmail.com>
2024-03-12 18:50:18 -0400

    adding employee

--- dev/server.js
+++ dev/server.js
@@ -64,4 +64,62 @@ inquirer
     });
     };
 
-    ShowAll
+ShowAllemployees = () => {
+console.log('Viewing All Employees');
+
+const sql = `
+SELECT 
+    employees.id,
+    employees.first_name,
+    employees.last_name,
+    employees.role_id,
+    employees.manager_id,
+    roles.title,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+FROM employees
+LEFT JOIN roles ON employees.role_id = roles.id
+LEFT JOIN employees manager ON employees.manager_id = manager.id
+`;
+
+db.query(sql, (err, rows) => {
+ if (err) throw err;
+console.table(rows);
+ promptUser();
+});
+};
+
+addEmployees = () => {
+console.log('Adding Employee');
+inquirer.prompt([
+{
+  type: 'input',
+  name: 'first_name',
+  message: 'What is the employee\'s first name?',
+  validate (value) {
+    if (value) {
+      return true;
+    } else {
+      console.log('Please enter the employee\'s first name.');
+      return false
+   }
+  }
+},
+{
+  type: 'input',
+  name: 'last_name',
+  message: 'What is the employee\'s last name?',
+  validate (value) {
+    if (value) {
+      return true;
+    } else {
+      console.log('Please enter the employee\'s last name.');
+      return false
+    }
+  }
+}
+])
+
+.then((answer) => {
+  const params = [answer.first_name, answer.last_name];
+
+const 
